4a95c18c345950ad29d296dd7f463c3775eb7f97
Eliott Delannay <Eliott@delannay.fr>
2020-01-29 10:41:14 +0100

    Generator : adding Peak paramaters from netCDF

--- CDataGenerator.hpp
+++ CDataGenerator.hpp
@@ -165,6 +165,64 @@ class CDataGenerator_Peak: public CDataGenerator<Tdata, Taccess>
 
 public:
 
+int Get_Graph_Parameters(int &nb_tB, int &nb_tA, double &tau, int &A, int &B){
+  ///file name
+  std::string fi="parameters.nc";//=cimg_option("-p","parameters.nc","comment");
+  int Tau;
+  ///parameter class
+  CParameterNetCDF fp;
+  //open file
+  int error=fp.loadFile((char *)fi.c_str());
+  if(error){std::cerr<<"loadFile return "<< error <<std::endl;return error;}
+
+  float process; 
+  std::string process_name="graph";
+  //load process variable
+  error=fp.loadVar(process,&process_name);
+  if(error){std::cerr<<"loadVar return "<< error <<std::endl;return error;}
+  std::cout<<process_name<<"="<<process<<std::endl;
+  ///nb_tB
+  std::string attribute_name="nb_tB";
+  if (error = fp.loadAttribute(attribute_name,nb_tB)!=0){
+    std::cerr<< "Error while loading "<<process_name<<":"<<attribute_name<<" attribute"<<std::endl;
+    return error;
+  }
+  std::cout<<"  "<<attribute_name<<"="<<nb_tB<<std::endl;
+
+  ///nb_tA
+  attribute_name="nb_tA";
+  if (error = fp.loadAttribute(attribute_name,nb_tA)!=0){
+    std::cerr<< "Error while loading "<<process_name<<":"<<attribute_name<<" attribute"<<std::endl;
+    return error;
+  }
+  std::cout<<"  "<<attribute_name<<"="<<nb_tA<<std::endl;
+
+  ///tau
+  attribute_name="tau";
+  if (error = fp.loadAttribute(attribute_name,Tau)!=0){
+    std::cerr<< "Error while loading "<<process_name<<":"<<attribute_name<<" attribute"<<std::endl;
+    return error;
+  }
+  std::cout<<"  "<<attribute_name<<"="<<Tau<<std::endl;
+  ///A
+  attribute_name="A";
+  if (error = fp.loadAttribute(attribute_name,A)!=0){
+    std::cerr<< "Error while loading "<<process_name<<":"<<attribute_name<<" attribute"<<std::endl;
+    return error;
+  }
+  std::cout<<"  "<<attribute_name<<"="<<A<<std::endl;	
+	 
+  ///B
+  attribute_name="B";
+  if (error = fp.loadAttribute(attribute_name,B)!=0){
+    std::cerr<< "Error while loading "<<process_name<<":"<<attribute_name<<" attribute"<<std::endl;
+    return error;
+  }
+  std::cout<<"  "<<attribute_name<<"="<<B<<std::endl; 
+  tau=Tau;
+
+}//Get_Graph_Parameters
+
   CDataGenerator_Peak(std::vector<omp_lock_t*> &lock
   , CDataAccess::ACCESS_STATUS_OR_STATE wait_status=CDataAccess::STATUS_FREE
   , CDataAccess::ACCESS_STATUS_OR_STATE  set_status=CDataAccess::STATUS_FILLED
@@ -194,17 +252,24 @@ public:
     this->laccess.wait_for_status(access[n],this->wait_status,this->STATE_FILLING, c);//free,filling
 
     //fill image with Peak
-//! \todo [high] fill images with local defined variables
     //Signal Parameters
-	const int nb_tB = 10000/10; // 10us
-	const int nb_tA = 100/10 + nb_tB; //100 ns
-	const float tau = 5000/10; //5 us
-	const int A =1234;
-	const int B=20;
-  //      Get_Graph_parameters(int &nb_tB, int &nb_tA, double &tau, int, A, int B, int SetSetupScope)
+	int nb_tB; // 10us
+	int nb_tA; //100 ns
+	double tau; //5 us
+	int A;
+	int B;
+        Get_Graph_Parameters(nb_tB, nb_tA, tau, A, B);
+	std::cerr<<nb_tA<<std::endl; 
+        nb_tA+=nb_tB;
+std::cerr<<nb_tB<<std::endl;
+std::cerr<<nb_tA<<std::endl;
+std::cerr<<tau<<std::endl;
+std::cerr<<A<<std::endl;
+	std::cerr<<B<<std::endl;
+
+
 
 	//Baseline
-
 	cimg_for_inX(images[n],0,nb_tB,i) images[n](i)=B;
         //Peak
 	const float step =(float)A/(nb_tA - nb_tB);
