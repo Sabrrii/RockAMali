833cef1e28e8b8145f1f1085a6da19c7faab5dac
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-02-21 09:29:26 +0100

    timing: std::chrono::high_resolution_clock

--- std_high_res_clock.cpp
+++ std_high_res_clock.cpp
@@ -6,17 +6,17 @@
 
 int main ()
 {
-  using namespace std::chrono;
+//  using namespace std::chrono;
 
-  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
   std::cout << "printing out 1000 stars...\n";
   for (int i=0; i<1000; ++i) std::cout << "*";
   std::cout << std::endl;
 
-  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
-  duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+  std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
 
   std::cout << "It took me " << time_span.count() << " seconds.";
   std::cout << std::endl;
