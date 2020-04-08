0429d380e2de17441c352aa753b652d29d2448c6
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-04-08 15:02:40 +0000

    profiling: full vMcPc on GPU

--- profiling.sh
+++ profiling.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ns=12
+ns=123
 
 list=
-for g in program_template program_T4 program_T4xyzw program_T4ls_fma
+for g in copy program lambda closure function function_lambda function_macro program_template program_T4 program_T4xyzw program_T4ls_fma
 do
   ./process_sequential -s 8192 -o sample_sequential.nc -r result_sequential.nc --generator-factory count -n $ns --use-GPU --GPU-factory $g
   ncks -A profiling_gpu.nc profiling_process.nc
@@ -13,3 +13,4 @@ do
 done
 
 #ensemble cat
+ncecat -O $list -o profiling_GPU.nc
