2b37dad31595961ebe277fe2fe4e7623dbdc9383
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-04-09 07:58:10 +0000

    profiling: script add attribute for kernel infos: # factory_name class_name

--- profiling.sh
+++ profiling.sh
@@ -3,13 +3,25 @@
 ns=123
 
 list=
+i=0
+#GPU kernels
 for g in copy program lambda closure function function_lambda function_macro program_template program_T4 program_T4xyzw program_T4ls_fma
 do
   ./process_sequential -s 8192 -o sample_sequential.nc -r result_sequential.nc --generator-factory count -n $ns --use-GPU --GPU-factory $g
+  #gather profiling infos
   ncks -A profiling_gpu.nc profiling_process.nc
   fo=profiling_GPU_$g.nc
   mv profiling_process.nc $fo
+  #appending file to list
   list=$list" "$fo
+
+  #tune (optional)
+  ##add kernel_# attribute
+  kernel=`ncdump -h $fo | grep iteration:kernel | cut -d'"' -f2`
+  ncatted -a kernel_$i,iteration,c,c,$i'_'$g'_'$kernel $fo
+
+  #next iteration
+  ((++i))
 done
 
 #ensemble cat
