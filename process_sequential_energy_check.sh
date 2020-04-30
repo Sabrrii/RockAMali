a5b3a8c234bac9a10442482bca2dc5c9c060bd82
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-04-30 11:55:40 +0200

    check: energy measurment check test (warning: static norm. for this data !!)

--- process_sequential_energy_check.sh
+++ process_sequential_energy_check.sh
@@ -6,7 +6,15 @@ gen="signal_pac_rnd"
 proc_list="pac filter"
 ns=23
 
-stats='maximum minimum mean'
+statistics='maximum minimum mean'
+
+#short names for statistics
+#stats='maxi mini mean'
+stats=
+for o in $statistics
+do
+  stats=$stats' '${o:0:4}
+done
 
 echo 'parse a few factory entities to compare to "'$gen'" simulation:'
 stat_list=
@@ -29,14 +37,16 @@ do
 
   #statistics
   list=
-  for o in $stats
+  i=1
+  for o in $statistics
   do
     fo=$fb'_'$o.nc
     list=$list' '$fo
     ncwa -y $o -O $fb'_diff.nc' -o $fo
-    var=E_${o:0:4}
+    var=E_`echo $stats | cut -d' ' -f$i`
     ncrename -v E,$var $fo
     ncatted -a long_name,$var,m,c,"$o energy" $fo
+    ((++i))
     ##show
     ncdump $fo
   done
@@ -64,7 +74,7 @@ done
 ##prepare
 fo=parameters_stats.nc
 ###extract E
-E=`ncdump -v signal parameters.nc | grep ':A ' | cut -d':' -f2 | sed 's/A/E/'`
+E=`ncdump -v signal parameters.nc | grep ':A ' | cut -d':' -f2 | sed 's/A/E/' | sed 's/ ;/f ;/'`
 ncap2 -O -s "$E" parameters.nc -o tmp.nc
 ncks -O -v E tmp.nc -o parameters_E.nc
 rm tmp.nc
@@ -72,26 +82,44 @@ rm tmp.nc
 rm parameters_stats.nc
 for o in $stats
 do
-  var=E_${o:0:4}
+  var=E_$o
   ncrename -O -v E,$var parameters_E.nc -o tmp.nc
   ncks -A tmp.nc -o $fo
 done
 rm tmp.nc parameters_E.nc
 ##clear history
 ncatted --history -a history,global,d,, -a history_of_appended_files,global,d,, $fo
+##show
 ncdump $fo
 
 ##%
 for f in $stat_list
 do
   fo=`basename $f .nc`_percentage.nc
-  ncap2 -O -s 'E_maxi*=1000;E_mini*=1000;E_mean*=1000;' $f -o tmp.nc
+  #vars='E_maxi*=100;E_mini*=100;E_mean*=100;'
+  vars=
+  for o in $stats
+  do
+    vars=$vars'E_'$o'*=100; '
+  done
+  ncap2 -O -s "$vars" $f -o tmp.nc
   ncbo -O --op_typ='/' tmp.nc parameters_stats.nc -o $fo
+  ##percentage
+  for o in $stats
+  do
+    var=E_$o
+    varp=E_$o'_%'
+    ncrename -v $var,$varp $fo
+    ncatted -a units,$varp,m,c,"%" $fo
+    ncatted -a long_name,$varp,a,c," percentage" $fo
+  done
+  ##clear history
+  ncatted --history -a history,global,d,, -a history_of_appended_files,global,d,, $fo
+  ##show
   ncdump $fo
 done
-rm tmp.nc
 #clean
-#rm parameters_stats.nc
+rm tmp.nc parameters_stats.nc
 
 #o. do a loop on processor (CPU factory, GPU ...)
 
