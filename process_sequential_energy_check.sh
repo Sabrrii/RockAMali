3b6f3aadad6f460c966aab063d62b518d0893811
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-04-30 09:18:04 +0200

    check: energy measurment check (single processor)

--- process_sequential_energy_check.sh
+++ process_sequential_energy_check.sh
@@ -3,20 +3,24 @@
 #usage: ./process_sequential_energy_check.sh
 #note: should be run after make process_sequential_run
 
+#prepare
 ncrename -d dim1,dimS pac_signal_parameters.nc
-ncdiff -O result_sequential.nc pac_signal_parameters.nc -o process_sequential_energy_check_diff.nc
-ncdump process_sequential_energy_check_diff.nc
+
+#difference
+fb=process_sequential_energy_check
+fo=$fb'_diff.nc'
+ncdiff -O result_sequential.nc pac_signal_parameters.nc -o $fo
+ncdump $fo
 grep -e 'signal:A=' -e 'signal:B=' parameters.cdl --color
 
-#check zero with nco
-fb=process_sequential_energy_check
+#statistics
 list=
-for o in max min avg
+for o in maximum minimum mean
 do
   fo=$fb'_'$o.nc
   list=$list' '$fo
   ncwa -y $o -O $fb'_diff.nc' -o $fo
-  var=E_$o
+  var=E_$o #todo: cut to 4 char length
   ncrename -v E,$var $fo
   ncatted -a long_name,$var,m,c,"$o energy" $fo
   ncdump $fo
@@ -31,6 +35,8 @@ done
 #clean history
 ncatted --history -a history,global,d,, -a history_of_appended_files,global,d,, $fo
 ncdump $fo
+
+#check zero
 
 #o. do a loop on processor (CPU factory, GPU ...)
 
