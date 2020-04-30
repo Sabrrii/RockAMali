777b379d49b75145e88268385a7e806388cd1952
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-04-30 09:30:01 +0200

    check: energy measurment check (processor loop) ; WiP: need normalisation of E for filter

--- process_sequential_energy_check.sh
+++ process_sequential_energy_check.sh
@@ -1,42 +1,60 @@
 #!/bin/bash
 
 #usage: ./process_sequential_energy_check.sh
-#note: should be run after make process_sequential_run
 
-#prepare
-ncrename -d dim1,dimS pac_signal_parameters.nc
+gen=signal_pac_rnd
+proc_list="pac filter"
+ns=23
 
-#difference
-fb=process_sequential_energy_check
-fo=$fb'_diff.nc'
-ncdiff -O result_sequential.nc pac_signal_parameters.nc -o $fo
-ncdump $fo
-grep -e 'signal:A=' -e 'signal:B=' parameters.cdl --color
-
-#statistics
-list=
-for o in maximum minimum mean
+echo 'parse a few factory entities to compare to "'$gen'" simulation:'
+stat_list=
+for p in $proc_list
 do
-  fo=$fb'_'$o.nc
-  list=$list' '$fo
-  ncwa -y $o -O $fb'_diff.nc' -o $fo
-  var=E_$o #todo: cut to 4 char length
-  ncrename -v E,$var $fo
-  ncatted -a long_name,$var,m,c,"$o energy" $fo
+  echo 'processor '$p':'
+  #run
+  ./process_sequential -s 4096 -o sample_sequential.nc -r result_sequential.nc --generator-factory $gen --CPU-factory $p -n $ns #--use-GPU --GPU-factory
+
+  #prepare
+  ncrename -d dim1,dimS pac_signal_parameters.nc
+
+  #difference
+  fb=process_sequential_energy_check
+  fo=$fb'_diff.nc'
+  ncdiff -O result_sequential.nc pac_signal_parameters.nc -o $fo
   ncdump $fo
+  grep -e 'signal:A=' -e 'signal:B=' parameters.cdl --color
+
+  #statistics
+  list=
+  for o in maximum minimum mean
+  do
+    fo=$fb'_'$o.nc
+    list=$list' '$fo
+    ncwa -y $o -O $fb'_diff.nc' -o $fo
+    var=E_$o #todo: cut to 4 char length
+    ncrename -v E,$var $fo
+    ncatted -a long_name,$var,m,c,"$o energy" $fo
+    ncdump $fo
+  done
+  ###gather
+  fo=process_sequential_energy_check_stats_$p.nc
+  rm $fo
+  for f in $list
+  do
+    ncks -A $f -o $fo
+  done
+  ###clean history
+  ncatted --history -a history,global,d,, -a history_of_appended_files,global,d,, $fo
+  ncdump $fo
+  stat_list=$stat_list' '$fo
 done
 
-fo=process_sequential_energy_check_stats.nc
-rm $fo
-for f in $list
+for f in $stat_list
 do
-  ncks -A $f -o $fo
+  ncdump $f
 done
-#clean history
-ncatted --history -a history,global,d,, -a history_of_appended_files,global,d,, $fo
-ncdump $fo
 
-#check zero
+#check close to zero
 
 #o. do a loop on processor (CPU factory, GPU ...)
 
--- process_sequential_vMcPc_check.sh
+++ process_sequential_vMcPc_check.sh
@@ -5,3 +5,4 @@ for f in program lambda closure function function_lambda function_macro program_
 do
   ./process_sequential -s 4096 -o sample_sequential.nc -r result_sequential.nc --generator-factory count --CPU-factory kernel -n 12 --use-GPU --GPU-factory $f --do-check 2>/dev/null | grep -e 'test:' -e max
 done
+
