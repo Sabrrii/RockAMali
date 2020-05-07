ed526d525a22a84a63f8b52181407704e9d81412
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2020-05-07 09:48:12 +0200

    CI: add CI temporary script, e.g. for CImg lib. as no SSH/git possible

---
+++ ci-temporary.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#temporary stuff for CI, i.e. within Linux docker container
+
+pwd
+ls ..
+ls ../*
+
+#dev. libs
+##CImg
+if [ ! -e ../CImg ]
+then
+  mv CImg ..
+else
+  rm -fr CImg
+fi
+
+##NetCDF
+#TODO
+
